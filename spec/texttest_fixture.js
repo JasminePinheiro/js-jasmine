4d02c4fc1dcb8aae90303672cf633fa5d4cddcb6
Jasmine Pinheiro <jasmine.pinheiero@aluno.ifsp.edu.br>
2023-11-18 17:12:48 -0300

    criação de classes para cada item

--- spec/texttest_fixture.js
+++ spec/texttest_fixture.js
@@ -1,31 +1,11 @@
-// arquivo de testes
-
-// qualidade não pode ser maior que 50
-const { Shop, Item } = require("../src/gilded_rose"); // importa as classes 'Shop' 'Item'
-
-// array de objetos representando diferentes tipos de produtos
-// const items = [
-//   new Item("+5 Dexterity Vest", 10, 20), // numero de dias pra vende-los e a qualidade
-//   new Item("Aged Brie", 2, 0), // o Queijo Brie aumenta a qualidade a medida que ele envelhece
-//   new Item("Elixir of the Mongoose", 5, 7),
-//   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-//   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-
-//   // This Conjured item does not work properly yet
-//   new Item("Conjured Mana Cake", 3, 6),
-//   new Item("Conjured Sulfuras, Hand of Ragnaros", 0, 80),
-
-// ];
+const { Shop, Item } = require("../src/gilded_rose");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
   new Item("+5 Dexterity Vest", -1, 20), // vencida
   new Item("+5 Dexterity Vest", 10, 51),
 
-  new Item("Aged Brie", 2, 0), 
+  new Item("Aged Brie", 2, 0),
 
   new Item("Elixir of the Mongoose", 5, 7),
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
@@ -47,19 +27,18 @@ const items = [
 
   new Item("Conjured Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Conjured Sulfuras, Hand of Ragnaros", -1, 80),
-
 ];
 
+const days = Number(process.argv[2]) || 2;
+const gildedRose = new Shop(items);
 
-
-const days = Number(process.argv[2]) || 2; // Obtém o número de dias a serem simulados a partir dos argumentos da linha de comando ou assume 2 dias como padrão 
-const gildedRose = new Shop(items); // crie uma instancia da loja com os itens fornecidos 
-
-// Um loop que itera sobre cada dia simulado, imprimindo o estado dos itens antes e depois de chamar updateQuality para cada dia.
 console.log("OMGHAI!");
 for (let day = 0; day < days; day++) {
   console.log(`\n-------- day ${day} --------`);
   console.log("name, sellIn, quality");
-  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+  gildedRose.items.forEach(item =>
+    console.log(`${item.name} | ${item.sellIn} | ${item.quality}`)
+  );
   gildedRose.updateQuality();
 }
+console.log("-------- Fim do Teste --------");
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,78 +4,110 @@ class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  updateQuality() {
+    throw new Error('O método updateQuality deve ser implementado nas classes filhas.');
+  }
+}
+
+class AgedBrieItem extends Item {
+  updateQuality() {
+    if (this.quality < 50) {
+      this.quality += 1;
+    }
+  }
+}
+
+class BackstagePassItem extends Item {
+  updateQuality() {
+    if (this.quality < 50) {
+      this.quality += 1;
+      if (this.sellIn < 11 && this.quality < 50) {
+        this.quality += 1;
+      }
+      if (this.sellIn < 6 && this.quality < 50) {
+        this.quality += 1;
+      }
+    }
+    if (this.sellIn < 0) {
+      this.quality = 0;
+    }
+  }
+}
+
+class SulfurasItem extends Item {
+  updateQuality() {
+    if (this.quality !== 80) {
+      this.quality = 80;
+    }
+  }
+}
+
+
+class ConjuredItem extends Item {
+  updateQuality() {
+    const isConjured = this.name.includes('Conjured');
+    const qualityDecrease = isConjured ? 2 : 1; // Diminui em dobro se for Conjured, caso contrário, diminui normalmente
+    if (this.quality > 0) {
+      this.quality -= qualityDecrease;
+    }
+
+    this.quality = Math.max(0, this.quality);
+  }
+}
+
+
+
+class ConjuredSulfurasItem extends SulfurasItem {
+  // Item lendário: qualidade imutável
+}
+
+class NormalItem extends Item {
+  updateQuality() {
+    const qualityDecrease = this.sellIn < 0 ? 2 : 1;
+    if (this.quality > 0) {
+      this.quality -= qualityDecrease;
+    }
+
+    this.quality = Math.max(0, this.quality);
+    this.quality = Math.min(50, this.quality);
+  }
 }
 
 class Shop {
   constructor(items = []) {
-    this.items = items;
+    this.items = items.map(item => this.createItem(item));
+  }
+
+  createItem(item) {
+    switch (item.name) {
+      case 'Aged Brie':
+        return new AgedBrieItem(item.name, item.sellIn, item.quality);
+
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        return new BackstagePassItem(item.name, item.sellIn, item.quality);
+
+      case 'Sulfuras, Hand of Ragnaros':
+        return new SulfurasItem(item.name, item.sellIn, item.quality);
+
+      case 'Conjured Sulfuras, Hand of Ragnaros':
+        return new ConjuredSulfurasItem(item.name, item.sellIn, item.quality);
+
+      case item.name.includes('Conjured'):
+        return new ConjuredItem(item.name, item.sellIn, item.quality);
+
+      default:
+        return new NormalItem(item.name, item.sellIn, item.quality);
+    }
   }
 
   updateQuality() {
-
-    for (var i = 0; i < this.items.length; i++) {
-      const isExpiration = this.items[i].name.includes('Conjured'); // se o item for expirado e conjurado ele diminui duas vezes mais rápido
-
-      switch (this.items[i].name) {
-        case 'Aged Brie':
-          if (this.items[i].quality < 50) {
-            this.items[i].quality += 1;
-          }
-          break;
-
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          if (this.items[i].quality < 50) {
-            this.items[i].quality += 1;
-            if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
-            if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
-          }
-          if (this.items[i].sellIn < 0) {
-            this.items[i].quality = 0;
-          }
-          break;
-
-        case 'Sulfuras, Hand of Ragnaros':
-          if (this.items[i].quality != 80) {
-            this.items[i].quality = 80;
-          }
-          break;
-
-        case 'Conjured Sulfuras, Hand of Ragnaros': // item lendário vão ter uma qualidade imutavel
-          if (this.items[i].quality != 80) {
-            this.items[i].quality = 80;
-          }
-          break;
-
-        default:
-          if (this.items[i].quality > 0) {
-            const qualityDecrease = isExpiration ? 2 : 1;
-            this.items[i].quality -= qualityDecrease;
-          }
-          if (this.items[i].sellIn < 0) {
-            const qualityDecreaseAfterExpiration = isExpiration ? 2 : 1;
-            this.items[i].quality -= qualityDecreaseAfterExpiration;
-
-          }
-
-          if (this.items[i].quality < 0) {
-            this.items[i].quality = 0;
-          }
-
-          if (this.items[i].quality > 50) {
-            this.items[i].quality = 50;
-          }
-
-          break;
+    this.items.forEach(item => {
+      item.updateQuality();
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn -= 1;
       }
-
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn -= 1;
-      }
-    }
+    });
 
     return this.items;
   }
